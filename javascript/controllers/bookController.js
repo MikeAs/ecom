5288fc26cd8d47f42e386fa7d26d4e672376f2ce
Assoune Mike <assoune.mike@gmail.com>
2016-03-16 04:18:05 +0100

    Modif product to book

---
+++ javascript/controllers/bookController.js
@@ -0,0 +1,13 @@
+angular.module('ecomWebsite')
+.controller('bookController', ['$scope', 'getDatas', function($scope, getDatas) {
+	$scope.pageTitle = 'Step 2 : Sélection de produits';
+
+	var that = this;
+	this.datas = {};
+
+	// get data from store json
+	var promise = getDatas.books();
+	promise.then(function(data) {
+	    that.datas = data;
+	});
+}]);
