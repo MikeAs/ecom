205d974c9d94072b5aaa2c235ec1c58be99faf05
Assoune Mike <assoune.mike@gmail.com>
2016-03-16 23:52:57 +0100

    Total price calcul

--- javascript/controllers/bookController.js
+++ javascript/controllers/bookController.js
@@ -5,6 +5,15 @@ angular.module('ecomWebsite')
 	var that = this;
 	this.datas = {};
 
+	this.calcTotal = function() {
+		var total = 0;
+		for(key in that.datas) {
+			var book = that.datas[key];
+			total += book.price * parseInt(book.qty);
+		}
+		return total;
+	}
+
 	// get data from book json
 	var promise = getDatas.books();
 	promise.then(function(data) {
